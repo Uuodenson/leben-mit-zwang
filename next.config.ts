b6bfdfe78074276f2213e19597318a73f809f30d
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2025-01-23 22:44:20 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/leben-mit-zwang/' : '',
-  basePath: isProd ? '/leben-mit-zwang' : '',
+  basePath: "/leben-mit-zwang",
   output: 'export'
 };
 

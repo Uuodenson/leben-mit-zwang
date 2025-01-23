a4ed60ed2a72f0e7d949f518a5517e9c08ea7f2d
Uuodenson <108183705+Uuodenson@users.noreply.github.com>
2025-01-23 22:31:59 +0100

    testing for alpha

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/leben-mit-zwang/' : '',
+  basePath: isProd ? '/leben-mit-zwang' : '',
+  output: 'export'
 };
 
 export default nextConfig;
